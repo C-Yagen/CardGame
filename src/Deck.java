8273f21424a7ff8dafdc671f9c525ee4bbf18f2e
connoryagen <connor.yagen@menloschool.org>
2024-11-15 11:00:58 -0800

    started constructor for deck

---
+++ src/Deck.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Deck {
+
+    private ArrayList<Card> cards;
+    private int cardsLeft;
+
+    public Deck(String[] ranks, String[] suits, int[] values) {
+        this.cardsLeft = 0;
+        this.cards = new ArrayList<Card>();
+        for (int i = 0; i < ranks.length; i++){
+            for (int j = 0; j < suits.length; j++){
+                this.cards.add(Card(ranks[i], suits[j], values[i]))
+                this.cardsLeft++;
+            }
+        }
+    }
+}
